d90bf835979b59739c8bd34fab0488b50e8f1126
fauzirramadan <fauzirramadan@gmail.com>
2022-08-16 00:14:21 +0700

    on boarding ui

--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -1,3 +1,5 @@
+import 'package:alquran_app_id/app/data/constant.dart';
+import 'package:alquran_app_id/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -9,16 +11,66 @@ class IntroductionView extends GetView<IntroductionController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('IntroductionView'),
-        centerTitle: true,
+        body: Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 70,
+          ),
+          const Text(
+            "Quran App",
+            style: TextStyle(
+                fontSize: 28, fontWeight: FontWeight.bold, color: purple1),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Container(
+            width: 195,
+            child: const Text(
+              "Learn Quran and Recite once everyday",
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(fontSize: 18, color: grey),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.center,
+            children: [
+              Container(
+                  height: 400,
+                  child: Image.asset("assets/images/onboarding_image.png")),
+              Positioned(
+                bottom: -20,
+                child: Container(
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    height: 60,
+                    minWidth: 185,
+                    color: pink,
+                    onPressed: () {
+                      Get.offAndToNamed(Routes.HOME);
+                    },
+                    child: const Text(
+                      "GET STARTED",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'IntroductionView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: Routes.INTRODUCTION,
       getPages: AppPages.routes,
     ),
   );
